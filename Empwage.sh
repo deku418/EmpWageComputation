d7522a09979e3662546e902e15e6b3cff1f164b6
deku418 <mayurvarade397@gmail.com>
2021-03-29 11:15:45 +0530

    Add_case

--- Empwage.sh
+++ Empwage.sh
@@ -6,18 +6,18 @@ Fulltime=1
 Parttime=2
 empPerHrs=20
 random=$((RANDOM%3))
-if [ $random -eq $Fulltime ]
-then
-	echo "Employee is full time present"
-	empHrs=8
-	salary=$((empPerHrs*empHrs))
-	elif [ $random -eq $Parttime ]
-	then
-   echo "Employee is half time present"
-   empHrs=4
-   salary=$((empPerHrs*empHrs))
-else
-	echo "Employee is Absent"
-	salary=0
-fi
+case $random in
+	$Fulltime )
+		echo "Employee is full time present"
+		empHrs=8
+	;;
+	$Parttime )
+   	echo "Employee is half time present"
+   	empHrs=4
+	;;
+	* )
+		echo "Employee is Absent"
+		empHrs=0
+esac
+salary=$((empPerHrs*empHrs))
 echo "Daily Salary of Employee : $salary"
