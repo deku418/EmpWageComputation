6af9b7dd821b793c9acd3ca88c991ba47b1bf38b
deku418 <mayurvarade397@gmail.com>
2021-03-29 12:24:33 +0530

    Add_uc5_MonthlyWage git commit -m Add_MonthlyWage git commit -m Add_uc5_MonthlyWage

--- Empwage.sh
+++ Empwage.sh
@@ -5,19 +5,22 @@ echo "Welcome to EmpWage Computation Problem"
 Fulltime=1
 Parttime=2
 empPerHrs=20
-random=$((RANDOM%3))
-case $random in
-	$Fulltime )
-		echo "Employee is full time present"
-		empHrs=8
-	;;
-	$Parttime )
-   	echo "Employee is half time present"
-   	empHrs=4
-	;;
-	* )
-		echo "Employee is Absent"
-		empHrs=0
-esac
-salary=$((empPerHrs*empHrs))
-echo "Daily Salary of Employee : $salary"
+workingDayPerMonth=20
+for ((i=1;i<=workingDayPerMonth;i++))
+do
+	random=$((RANDOM%3))
+	case $random in
+		$Fulltime )
+			empHrs=8
+		;;
+		$Parttime )
+   		empHrs=4
+		;;
+		* )
+			empHrs=0
+	esac
+		salary=$((empPerHrs*empHrs))
+		totalsalary=$((salary+totalsalary))
+done
+echo "Daily salary of Employee: $salary"
+echo "Monthly salary of Employee: $totalsalary"
