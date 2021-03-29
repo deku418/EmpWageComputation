bfc3248277fd1c0a36237fea6dd6ec3a5a1a6e0b
deku418 <mayurvarade397@gmail.com>
2021-03-29 12:46:22 +0530

    Add_Wagesfor100Hr

--- Empwage.sh
+++ Empwage.sh
@@ -6,7 +6,10 @@ Fulltime=1
 Parttime=2
 empPerHrs=20
 workingDayPerMonth=20
-for ((i=1;i<=workingDayPerMonth;i++))
+totalHr=0
+totalMaxHr=100
+totalworkingdays=0
+while [ $totalHr -le $totalMaxHr ]
 do
 	random=$((RANDOM%3))
 	case $random in
@@ -19,8 +22,9 @@ do
 		* )
 			empHrs=0
 	esac
-		salary=$((empPerHrs*empHrs))
+		totalHr=$((totalHr+empHrs))
+		salary=$((empPerHrs*totalHr))
 		totalsalary=$((salary+totalsalary))
 done
 echo "Daily salary of Employee: $salary"
-echo "Monthly salary of Employee: $totalsalary"
+echo "Salary of Employee for 100 hrs: $totalsalary"
