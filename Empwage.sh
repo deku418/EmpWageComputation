ce5e68b358f609bd8eae89f9cda7f90ec7d1941e
deku418 <mayurvarade397@gmail.com>
2021-03-29 10:54:38 +0530

    Add_EmployeeAttendance

--- Empwage.sh
+++ Empwage.sh
@@ -2,3 +2,11 @@
 
 echo "Welcome to EmpWage Computation Problem"
 
+EmployeePresent=1
+random=$((RANDOM%2))
+if [ $random -eq $EmployeePresent ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
