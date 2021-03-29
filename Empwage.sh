866a28b84bc25e40b894521ee29a6c63376c5c09
deku418 <mayurvarade397@gmail.com>
2021-03-29 11:02:59 +0530

    Add_DailyEmployeeWage

--- Empwage.sh
+++ Empwage.sh
@@ -3,10 +3,15 @@
 echo "Welcome to EmpWage Computation Problem"
 
 EmployeePresent=1
+empPerHrs=20
 random=$((RANDOM%2))
 if [ $random -eq $EmployeePresent ]
 then
 	echo "Employee is Present"
+	empHrs=8
+	salary=$((empPerHrs*empHrs))
 else
 	echo "Employee is Absent"
+	salary=0
 fi
+echo "Daily Salary of Employee : $salary"
